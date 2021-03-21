7d1d04a8de6898533e83e9d652162b8469d8c797
Thanchanok Intamoon <57270184+x-sletor@users.noreply.github.com>
2021-03-22 01:22:24 +0700

    หน้าหลักไม่มีค้นหา มีลิ้ง
    มีลิงค์

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,22 @@ var express = require('express');
 var router = express.Router();
 var {Todos} = require('../models/todos');
 
+const home = (req, res, next) => {
+  if (!req.session.user) {
+      return res.redirect('/index')
+          //return res.redirect('/index')
+  }
+  next()
+}
+
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  const todos = await Todos.find();
-  // (description:'going to the gym', complete)
-  res.render('index', { title: 'Todos', todos: todos });
+  res.render('index', { title: 'Express', user: req.session.user });
+});
+
+/* page */
+router.get('/cashier', home, function(req,res,next){
+  res.render('cashier');
 });
 
 module.exports = router;
